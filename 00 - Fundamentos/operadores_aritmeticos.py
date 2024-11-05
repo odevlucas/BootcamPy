2db3ddc10cc1d804d4cab7badcf0e09d3e05eaf5
Lucas Silva <odeveloperlucas@gmail.com>
2024-11-05 19:53:41 -0300

    Reorganização das pastas e adição dos operadores de comparacao, aritmeticos e de atribuicao

---
+++ 00 - Fundamentos/operadores_aritmeticos.py
@@ -0,0 +1,10 @@
+valor_1 = 20
+valor_2 = 10
+
+print(valor_1 + valor_2,"- Soma")
+print(valor_1 - valor_2,"- Subtração")
+print(valor_1 / valor_2,"- Divisão")
+print(valor_1 // valor_2,"- Divisão Inteira" )
+print(valor_1 * valor_2,"- Multiplicação")
+print(valor_1 ** 3,"- Exponenciação")
+print(valor_1 % valor_2,"- Módulo")
---
+++ 00 - Fundamentos/operadores_atribuicao.py
@@ -0,0 +1,5 @@
+#A atribuição funcionará com todos os operadores
+saldo = 500
+print("valor inicial",saldo)
+saldo += 200 #adicionar o valor sem perder as informações salvas anteriormente
+print("valor com operador de atribuicao (+= 200)", saldo)
